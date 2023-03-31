b5a0ab22f036e4715fc45503e776a12eba62e663
413C5 <a_gonzalezzarate2@yahoo.com>
2023-03-31 11:11:28 -0600

    Avance Clase 3 P1

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Note from "./Note";
+
+const App = ({ notes }) => {
+    //const {notes}=props
+    //console.log(notes)
+    return (
+      <div>
+        <h1>Notes</h1>
+        <ul>
+          {notes.map((x) => {
+            console.log(x.id, x.content)
+            return (
+              <Note key={x.id} note={x} />
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
+
+export default App
